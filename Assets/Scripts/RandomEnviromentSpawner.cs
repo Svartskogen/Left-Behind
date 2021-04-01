3fa6a90c2f09dcb4cf41dfa3489678c9777f1486
Svartskogen <amichelidebard@gmail.com>
2021-03-31 23:35:44 -0300

    post processing, map details

---
+++ Assets/Scripts/RandomEnviromentSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEnviromentSpawner : MonoBehaviour
+{
+    public GameObject[] props;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(props[Random.Range(0, props.Length)], transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
