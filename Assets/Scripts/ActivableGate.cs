b0f3c670d87b978fa75b2303da3910ba3cbb9c05
Svartskogen <amichelidebard@gmail.com>
2021-04-01 01:40:29 -0300

    dust polish, gate and activator

---
+++ Assets/Scripts/ActivableGate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivableGate : MonoBehaviour
+{
+    public bool State { get; set; }
+    [SerializeField] Material offMaterial;
+    [SerializeField] Material onMaterial;
+    [SerializeField] bool initialState = false;
+
+    MeshRenderer displayRenderer;
+    private void Awake()
+    {
+        displayRenderer = transform.GetChild(0).GetComponent<MeshRenderer>();
+        SetStateTo(initialState);
+    }
+
+    public void SetStateTo(bool state)
+    {
+        State = state;
+        if (state)
+        {
+            displayRenderer.material = onMaterial;
+        }
+        else
+        {
+            displayRenderer.material = offMaterial;
+        }
+    }
+    [ContextMenu("SwitchState")]
+    public void SwitchState()
+    {
+        SetStateTo(!State);
+    }
+}
---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public ActivableGate[] keys;
+    public bool open = false;
+
+    Vector3 closePos;
+    Vector3 openPos;
+    float speed = 0.2f;
+    private void Start()
+    {
+        closePos = transform.position;
+        openPos = transform.position;
+        openPos.y -= 1;
+    }
+
+    private void Update()
+    {
+        if (open)
+        {
+            if(transform.position != openPos)
+            {
+                var dir = openPos - transform.position;
+                dir = dir.normalized;
+                transform.position = Vector3.MoveTowards(transform.position, openPos, Time.deltaTime * speed);
+            }
+        }
+        else
+        {
+            if (transform.position != closePos)
+            {
+                var dir = closePos - transform.position;
+                dir = dir.normalized;
+                transform.position = Vector3.MoveTowards(transform.position, closePos, Time.deltaTime * speed);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        open = CheckKeys();
+    }
+    bool CheckKeys()
+    {
+        foreach(ActivableGate key in keys)
+        {
+            if (!key.State)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
