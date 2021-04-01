f0c8d45c8cb044d434f3cf5e5881763daf01c7a6
Svartskogen <amichelidebard@gmail.com>
2021-03-31 22:05:47 -0300

    Rover movement, camera follow, lighting

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    GameObject target;
+    public Vector3 offset;
+    public float smoothTime = 0.3f;
+
+    private Vector3 velocity = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetPosition = target.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+        // update rotation
+        //transform.LookAt(target.transform);
+    }
+}
---
+++ Assets/Scripts/RoverMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoverMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float turnSpeed = 10f;
+
+    Rigidbody rigidbody;
+    Vector3 velocityHolder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        velocityHolder = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        velocityHolder = transform.forward * Input.GetAxis("Vertical") * speed;
+        rigidbody.velocity = velocityHolder;
+        float reverseMod = 1;
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            reverseMod = -1;
+        }
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * reverseMod, 0);
+    }
+}
