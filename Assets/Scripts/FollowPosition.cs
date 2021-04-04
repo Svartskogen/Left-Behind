81c91529e6798ec9b2798714c819931fe3c55213
Svartskogen <amichelidebard@gmail.com>
2021-04-03 22:21:20 -0300

    dust storm

---
+++ Assets/Scripts/FollowPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPosition : MonoBehaviour
+{
+    public Transform follow;
+    Vector3 offset;
+    
+    void Start()
+    {
+        offset = follow.position + transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = follow.position + offset;
+    }
+}
