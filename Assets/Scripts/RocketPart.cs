683fcc5458d2b25405cf57943c1a01d7f559d2fa
Svartskogen <amichelidebard@gmail.com>
2021-04-05 04:24:35 -0300

    rocket crafting skeleton

---
+++ Assets/Scripts/RocketPart.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//al acercarse al jugador, se le equipa y se le asigna como padre. se activa modo carry
+//al estar en modo carry, si se acerca a una Rocket Platform se agrega ese tipo de parte
+public class RocketPart : MonoBehaviour
+{
+    public Type type;
+    bool carring = false; //true si esta parte la esta llevando el jugador
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //if !carring and player: equip
+        //if carring and plaform: add
+    }
+    public enum Type
+    {
+        Base, Fuel, Sides, Fins, Top
+    }
+}
---
+++ Assets/Scripts/RocketPlatform.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketPlatform : MonoBehaviour, IInteractable
+{
+    public MeshRenderer display;
+    public Material onMaterial;
+    public Material offMaterial;
+
+    [SerializeField] MeshRenderer rocketBase;
+    [SerializeField] MeshRenderer rocketFuel;
+    [SerializeField] MeshRenderer rocketSides;
+    [SerializeField] MeshRenderer rocketFins;
+    [SerializeField] MeshRenderer rocketTop;
+    
+    void Awake()
+    {
+        rocketBase.enabled = false;
+        rocketFuel.enabled = false;
+        rocketSides.enabled = false;
+        rocketFins.enabled = false;
+        rocketTop.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsRocketComplete())
+        {
+            display.material = onMaterial;
+        }
+        else
+        {
+            display.material = offMaterial;
+        }
+    }
+    bool IsRocketComplete()
+    {
+        return (rocketBase.enabled && rocketFuel.enabled && rocketSides.enabled && rocketFins.enabled && rocketTop.enabled);
+    }
+    public void Interact()
+    {
+        if (IsRocketComplete())
+        {
+            //END GAME
+        }
+    }
+    public void AddPart(RocketPart.Type partType)
+    {
+        switch (partType)
+        {
+            case RocketPart.Type.Base:
+                rocketBase.enabled = true;
+                return;
+            case RocketPart.Type.Fuel:
+                rocketFuel.enabled = true;
+                return;
+            case RocketPart.Type.Sides:
+                rocketSides.enabled = true;
+                return;
+            case RocketPart.Type.Fins:
+                rocketFins.enabled = true;
+                return;
+            case RocketPart.Type.Top:
+                rocketTop.enabled = true;
+                return;
+        }
+    }
+}
